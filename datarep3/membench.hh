e980a747bbac118184a71ad69190890e4c317798
Eddie Kohler <ekohler@gmail.com>
2021-09-09 15:07:48 +0000

    Whoops

---
+++ datarep3/membench.hh
@@ -0,0 +1,11 @@
+#ifndef CS61_MEMBENCH_HH
+#define CS61_MEMBENCH_HH
+#include "hexdump.hh"
+#include <cstdio>
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+
+unsigned long memnode_benchmark(unsigned noperations, int seed);
+
+#endif
