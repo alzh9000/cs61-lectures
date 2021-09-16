df6515497e4e8f807ae551d7888296ce1f0b12e0
Eddie Kohler <ekohler@gmail.com>
2021-09-16 09:29:21 -0400

    More nits

--- datarep4/ubsignedinc.cc
+++ datarep4/ubsignedinc.cc
@@ -7,15 +7,15 @@
 int main(int argc, char** argv) {
     int x;
     if (argc >= 2) {
-	x = strtol(argv[1], nullptr, 0);
+        x = strtol(argv[1], nullptr, 0);
     } else {
-	fprintf(stderr, "Usage: %s NUMBER\n", argv[0]);
-	exit(1);
+        fprintf(stderr, "Usage: %s NUMBER\n", argv[0]);
+        exit(1);
     }
 
     assert(x + 1 > x);
     printf("%d + 1 > %d - assertion passed\n", x, x);
-    int x_incr = x + 1;
 
+    int x_incr = x + 1;
     printf("%d + 1 = %d\n", x, x_incr);
 }
