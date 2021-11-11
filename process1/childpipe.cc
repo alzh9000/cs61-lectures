f6137cef6771bc5f30f6cda39ab2faf788536c02
Eddie Kohler <ekohler@gmail.com>
2021-11-11 10:57:09 -0500

    New process1 programs, new storage1 program

--- process1/childpipe.cc
+++ process1/childpipe.cc
@@ -7,8 +7,8 @@
 
 int main() {
     // Create pipe
-    int pipefd[2];
-    int r = pipe(pipefd);
+    int pfd[2];
+    int r = pipe(pfd);
     assert(r == 0);
 
     pid_t p1 = fork();
@@ -16,27 +16,25 @@ int main() {
 
     if (p1 == 0) {
         // Child: Write message to pipe, then exit
-        const char* message = "Hello, mama!\n";
-        ssize_t nw = write(pipefd[1], message, strlen(message));
-        assert(nw == (ssize_t) strlen(message));
-        exit(0);
-
-    } else {
-        // Parent: Read all messages from child; exit when child
-        // is done
-        close(pipefd[1]); // Parent does not write!!!!
-        while (true) {
-            char buf[BUFSIZ];
-            ssize_t nr = read(pipefd[0], buf, sizeof(buf));
-            if (nr == 0
-                || (nr == -1 && errno != EINTR && errno != EAGAIN)) {
-                break;
-            } else if (nr > 0) {
-                printf("I got a message! It was “%.*s”\n", (int) nr, buf);
-            }
-        }
-        printf("No more messages :(\n");
-        close(pipefd[0]);
+        const char* msg = "Hello, mama!";
+        ssize_t nw = write(pfd[1], msg, strlen(msg));
+        assert(nw == (ssize_t) strlen(msg));
         exit(0);
     }
+
+    // Parent: Read all messages from child; exit when child
+    // is done
+    close(pfd[1]); // Parent does not write!!!!
+    while (true) {
+        char buf[BUFSIZ];
+        ssize_t nr = read(pfd[0], buf, sizeof(buf));
+        if (nr > 0) {
+            printf("Received message “%.*s”\n", (int) nr, buf);
+        } else if (nr == 0
+                   || (nr == -1 && errno != EINTR && errno != EAGAIN)) {
+            break;
+        }
+    }
+    printf("No more messages :(\n");
+    close(pfd[0]);
 }
